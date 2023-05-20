54c3f5c2be3092fd5497ad9596131edba651f992
Wiree <katak6565@gmail.com>
2023-05-20 11:44:50 +0700

    add integration test

---
+++ integration_test/app.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:my_app/main.dart' as app;
+import 'package:my_app/screens/landing_screen.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group(
+    'end to end test',
+    () {
+      testWidgets(
+        'verify login screen with correct username and password',
+        (tester) async {
+          app.main();
+          await tester.pumpAndSettle();
+          await Future.delayed(const Duration(seconds: 2));
+          await tester.enterText(
+              find.byType(TextField).at(0), 'katak1@gmail.com');
+          await Future.delayed(const Duration(seconds: 2));
+          await tester.enterText(find.byType(TextField).at(1), '12345678');
+          await Future.delayed(const Duration(seconds: 2));
+          await tester.tap(find.byType(ElevatedButton));
+          await Future.delayed(const Duration(seconds: 2));
+          await tester.pumpAndSettle();
+
+          await Future.delayed(const Duration(seconds: 2));
+          expect(find.byType(LandingScreen), findsOneWidget);
+        },
+      );
+
+      // testWidgets(
+      //   'verify login screen with incorrect username and password',
+      //   (tester) async {
+      //     app.main();
+      //     await tester.pumpAndSettle();
+      //     await Future.delayed(const Duration(seconds: 2));
+      //     await tester.enterText(
+      //         find.byType(TextFormField).at(0), 'katak1@gmail.com');
+      //     await Future.delayed(const Duration(seconds: 2));
+      //     await tester.enterText(find.byType(TextFormField).at(1), '12345678');
+      //     await Future.delayed(const Duration(seconds: 2));
+      //     await tester.tap(find.byType(ElevatedButton));
+      //     await Future.delayed(const Duration(seconds: 2));
+      //     await tester.pumpAndSettle();
+
+      //     await Future.delayed(const Duration(seconds: 2));
+      //     expect(find.text('Invalid username or password'), findsOneWidget);
+      //   },
+      // );
+    },
+  );
+}
